834f8d7c634a060588da0e6d19239bf7227855df
Dana <dana.hong@hotmail.com>
2023-05-24 19:51:08 -0400

    Format

--- src/App.tsx
+++ src/App.tsx
@@ -11,37 +11,40 @@ type Habit = {
   currentStreak: number;
   showMenu: boolean;
   // startDate: Date;
-}
+};
 
 function App() {
-  const [habitList, setHabitList] = useState<[] | Habit[]>(JSON.parse(localStorage.getItem('habitList') as string));
-  const [habitName, setHabitName] = useState<string>('');
-  const [previousDate] = useState<Date>(new Date(JSON.parse(localStorage.getItem('prevDate') as string)));
+  const [habitList, setHabitList] = useState<[] | Habit[]>(
+    JSON.parse(localStorage.getItem("habitList") as string)
+  );
+  const [habitName, setHabitName] = useState<string>("");
+  const [previousDate] = useState<Date>(
+    new Date(JSON.parse(localStorage.getItem("prevDate") as string))
+  );
 
   useEffect(() => {
     if (isYesterday(previousDate)) {
-      setHabitList(prevHabitList => {
-        return prevHabitList.map(habitItem => {
+      setHabitList((prevHabitList) => {
+        return prevHabitList.map((habitItem) => {
           if (habitItem.completed) {
             return {
               ...habitItem,
-              completed: false
-            }
+              completed: false,
+            };
           }
           return {
             ...habitItem,
-            currentStreak: 0
-          }
+            currentStreak: 0,
+          };
         });
       });
 
-      localStorage.setItem('prevDate', JSON.stringify(startOfDay(new Date(Date.now()))));
+      localStorage.setItem("prevDate", JSON.stringify(startOfDay(new Date(Date.now()))));
     }
-
   }, []);
 
   useEffect(() => {
-    localStorage.setItem('habitList', JSON.stringify(habitList));
+    localStorage.setItem("habitList", JSON.stringify(habitList));
   }, [habitList]);
 
   return (
@@ -49,68 +52,72 @@ function App() {
       <div className="h-screen w-screen bg-gray-900 flex justify-center items-center">
         <div>
           <div className="flex flex-col gap-2">
-            {
-              habitList.map((habitItem, habitIndex) =>
-                <div key={habitIndex}>
+            {habitList.map((habitItem, habitIndex) => (
+              <div key={habitIndex}>
+                <div
+                  onClick={() => {
+                    setHabitList((prevHabitList) => {
+                      return prevHabitList.map((habitItem, index) => {
+                        return habitIndex === index
+                          ? { ...habitItem, showMenu: !habitItem.showMenu }
+                          : habitItem;
+                      });
+                    });
+                  }}
+                  className="flex justify-between items-center p-2 bg-gray-300"
+                >
+                  <p className={habitItem.completed ? "line-through" : ""}>{habitItem.name}</p>
                   <div
-                    onClick={
-                      () => {
-                        setHabitList(prevHabitList => {
-                          return prevHabitList.map((habitItem, index) => {
-                            return (habitIndex === index) ? { ...habitItem, showMenu: !habitItem.showMenu } : habitItem
+                    onClick={() => {
+                      setHabitList((prevHabitList) => {
+                        return prevHabitList.map((habitItem, index) => {
+                          return habitIndex === index
+                            ? {
+                                ...habitItem,
+                                completed: !habitItem.completed,
+                                currentStreak: !habitItem.completed
+                                  ? habitItem.currentStreak + 1
+                                  : habitItem.currentStreak - 1,
+                              }
+                            : habitItem;
+                        });
+                      });
+                    }}
+                  >
+                    {habitItem.completed ? (
+                      <CheckBoxCircle className="h-6 w-6" />
+                    ) : (
+                      <EmptyCircle className="h-6 w-6" />
+                    )}
+                  </div>
+                </div>
+                <div
+                  className={`${
+                    habitItem.showMenu ? "" : "hidden"
+                  } p-2 border border-t-black bg-gray-300`}
+                >
+                  <div className="flex justify-between">
+                    <p>Skip</p>
+                    <div
+                      className="p-2 border border-gray-900 cursor-pointer"
+                      onClick={() =>
+                        setHabitList((prevHabitList) => {
+                          return prevHabitList.filter((habitItem, index) => {
+                            if (habitIndex !== index) return habitItem;
                           });
                         })
                       }
-                    }
-                    className="flex justify-between items-center p-2 bg-gray-300"
-                  >
-                    <p className={habitItem.completed ? 'line-through' : ''} >{habitItem.name}</p>
-                    <div
-                      onClick={
-                        () => {
-                          setHabitList(prevHabitList => {
-                            return prevHabitList.map((habitItem, index) => {
-                              return (habitIndex === index) ? { ...habitItem, completed: !habitItem.completed, currentStreak: !habitItem.completed ? habitItem.currentStreak + 1 : habitItem.currentStreak - 1 } : habitItem
-                            });
-                          })
-                        }
-                      }
-                      >
-                      {
-                        habitItem.completed ? 
-                        <CheckBoxCircle className="h-6 w-6" />
-                        :
-                        <EmptyCircle className="h-6 w-6"/>
-                      }
+                    >
+                      <p>Delete</p>
                     </div>
                   </div>
-                  <div
-                    className={`${habitItem.showMenu ? '' : 'hidden'} p-2 border border-t-black bg-gray-300`}
-                  >
-                    <div className="flex justify-between">
-                      <p>Skip</p>
-                      <div
-                        className="p-2 border border-gray-900 cursor-pointer"
-                        onClick={
-                          () =>
-                          setHabitList(prevHabitList => {
-                            return prevHabitList.filter((habitItem, index) => {
-                              if (habitIndex !== index) return habitItem
-                            });
-                          })
-                        }
-                        >
-                        <p>Delete</p>
-                      </div>
-                    </div>
-                    <p>Current Streak: {habitItem.currentStreak}</p>
-                  </div>
+                  <p>Current Streak: {habitItem.currentStreak}</p>
                 </div>
-              )
-            }
+              </div>
+            ))}
           </div>
           <div className="flex flex-col gap-2 mt-2">
-            <input 
+            <input
               value={habitName}
               onChange={(e) => setHabitName(e.target.value)}
               type="text"
@@ -120,23 +127,21 @@ function App() {
               className="px-1"
             />
             <button
-              onClick={
-                () => {
-                  if (!habitName) return;
-                  setHabitList(prevHabitList => 
-                    [...prevHabitList,
-                      { 
-                        name: habitName,
-                        completed: false,
-                        currentStreak: 0,
-                        showMenu: false
-                      } 
-                    ]);
-                  setHabitName('');
-                }
-              }
+              onClick={() => {
+                if (!habitName) return;
+                setHabitList((prevHabitList) => [
+                  ...prevHabitList,
+                  {
+                    name: habitName,
+                    completed: false,
+                    currentStreak: 0,
+                    showMenu: false,
+                  },
+                ]);
+                setHabitName("");
+              }}
               className="text-slate-200 bg-gray-700 py-2 px-4 rounded-lg border border-gray-800"
-              >
+            >
               Add new habit
             </button>
           </div>
